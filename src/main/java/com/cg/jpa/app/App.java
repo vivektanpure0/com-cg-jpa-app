7240e3bf33e85fb1ecf2ed3e18c61aff4581a340
vivektanpure0 <vivektanpure0@gmail.com>
2021-06-28 18:05:16 +0530

    Initial commit

---
+++ src/main/java/com/cg/jpa/app/App.java
@@ -0,0 +1,41 @@
+package com.cg.jpa.app;
+
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Start");
+		try {
+			Configuration config = new Configuration();
+			
+			SessionFactory factory = config.configure().buildSessionFactory();
+			Session session = factory.openSession();
+			
+			Transaction transaction = session.beginTransaction();
+			Employee emp = new Employee("Sonu", 10.5);
+			session.save(emp); // insert
+			transaction.commit();
+			Employee e1;
+			
+			e1 = session.get(Employee.class, 1); // select
+			System.out.println(e1.toString());
+			session.evict(e1);
+			
+//			e1.setName("Monu");
+//			session.update(e1); // update
+//			e1 = session.get(Employee.class, 101);
+//			System.out.println(e1.toString());
+//			session.delete(e1); // delete
+//			e1 = session.get(Employee.class, 101);
+//			System.out.println(e1.toString());
+			session.close();
+			factory.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		System.out.println("End");
+	}
+}
---
+++ src/main/java/com/cg/jpa/app/Employee.java
@@ -0,0 +1,66 @@
+package com.cg.jpa.app;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cg_emp")
+public class Employee {
+
+	@Id
+	@Column(name = "employee_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@Column(name = "employee_name", length = 128, nullable = false)
+	private String name;
+
+	@Column
+	private double salary;
+
+	public Employee(int id, String name, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public Employee(String name, double salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public Employee() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+}
